4c01b9741768a2a2e14c86479c3d1fd4bf6c2eea
ManavGhaiCode <manavghai12october@gmail.com>
2023-08-02 12:14:45 +0530

    add basic jump

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float speed = 5f;
-
-    private float moveInput;
+    public float JumpForce = 5f;
 
     private Rigidbody2D rb;
+
+    private float moveInput;
+    private bool isGrounded;
+    private bool isJumping = false;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -13,9 +16,12 @@ public class Player : MonoBehaviour {
 
     private void Update() {
         moveInput = Input.GetAxis("Horizontal");
-    }
+        isJumping = Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow);
 
-    void FixedUpdate() {
         rb.velocity = new Vector2 (moveInput * speed, rb.velocity.y);
+
+        if (isJumping) {
+            rb.velocity = new Vector2 (rb.velocity.x, (Vector2.up * JumpForce).y);
+        }
     }
 }
